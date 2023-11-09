0c4790322bece0e46eae61470f4c6dd9f4c228d5
Rostyslav <RostikL1994@gmail.com>
2023-11-09 15:03:29 +0300

    adding animation

--- src/js/script.js
+++ src/js/script.js
@@ -20,6 +20,16 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    const header = document.querySelector('.header');
+
+    window.onscroll = () => {
+        if (window.pageYOffset > 200) {
+            header.classList.add('sticky');
+        } else {
+            header.classList.remove('sticky');
+        }
+    };
+
     const swiper = new Swiper('.swiper', {
 
         slidesPerView: 2,
@@ -35,5 +45,36 @@ document.addEventListener('DOMContentLoaded', () => {
             nextEl: ".review__button_next",
             prevEl: ".review__button_prev",
         },
+    });
+
+    header.addEventListener('click', event => {
+        if (event.target.classList.contains('header__link')) {
+            event.preventDefault();
+
+            const targetId = event.target.getAttribute('href').slice(1);
+            const targetElement = document.getElementById(targetId);
+            const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+            const startPosition = window.pageYOffset;
+            const distance = targetPosition - startPosition;
+
+            let startTime = null;
+
+            function animation(currentTime) {
+                if (startTime === null) startTime = currentTime;
+                const timeElapsed = currentTime - startTime;
+                const run = easeInOutQuad(timeElapsed, startPosition, distance, 600);
+                window.scrollTo(0, run);
+                if (timeElapsed < 600) requestAnimationFrame(animation);
+            }
+
+            function easeInOutQuad(t, b, c, d) {
+                t /= d / 2;
+                if (t < 1) return c / 2 * t * t + b;
+                t--;
+                return -c / 2 * (t * (t - 2) - 1) + b;
+            }
+
+            requestAnimationFrame(animation);
+        }
     });
 });

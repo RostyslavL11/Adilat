63e3c77afe5aaf6074a2fe6310f8b9b98596ad30
Rostyslav <RostikL1994@gmail.com>
2023-11-09 13:08:04 +0300

    adding inputs

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,39 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    $('ul.works__tab').on('click', 'li:not(.works__btn_active)', function () {
-        $(this)
-            .addClass('works__btn_active').siblings().removeClass('works__btn_active')
-    })
+//     $('ul.works__tabs button').on('click', 'button:not(.works__btn_active)', function () {
+//         $(this)
+//             .addClass('works__btn_active')
+//             .siblings()
+//             .removeClass('works__btn_active')
+//     })
 
+// });
+
+// $(document).ready(function () {
+//     $('ul.works__tabs button').on('click', function () {
+//         $(this)
+//             .addClass('works__btn_active')
+//             .parent().siblings().find('button')
+//             .removeClass('works__btn_active');
+//     });
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const swiper = new Swiper('.swiper', {
+
+        slidesPerView: 2,
+        spanceBetween: 20,
+        pagination: {
+            el: '.review__dots',
+            bulletActiveClass: 'review__dots_dot-active',
+            bulletClass: 'review__dots_dot',
+            clickable: true
+        },
+
+        navigation: {
+            nextEl: ".review__button_next",
+            prevEl: ".review__button_prev",
+        },
+    });
 });
